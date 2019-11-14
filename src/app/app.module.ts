d4f4e2176a5a7fd32acbefed037373717eac5718
Diana Mounir <46133476+DianaMounir@users.noreply.github.com>
2019-11-14 12:11:05 +0200

    add bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AlertModule } from 'ngx-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
